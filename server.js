1e6c0876deefa58a16fa603a127b479c3754b702
DimaHaurylkevich <dhaurylkevich@gmail.com>
2025-03-05 16:50:51 +0100

    off bot

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const Manager = require("./src/index");
 const CarService = require("./src/services/carService");
-const bot = require("./src/bot");
+// const bot = require("./src/bot");
 const port = process.env.PORT || 3000;
 const cron = require('node-cron');
 
 const parsingTask = cron.schedule('*/5 * * * *', async () => {
-    console.log(`[${new Date().toISOString()}] Start parsing`);
-    await Manager.run(bot);
-});
+//     console.log(`[${new Date().toISOString()}] Start parsing`);
+//     await Manager.run(bot);
+// });
 
-const cleanupTask = cron.schedule('0 0 1 * *', async () => {
-    console.log("Executing monthly cleanup...");
-    await CarService.clear();
-});
+// const cleanupTask = cron.schedule('0 0 1 * *', async () => {
+//     console.log("Executing monthly cleanup...");
+//     await CarService.clear();
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
